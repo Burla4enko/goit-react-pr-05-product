84db6e353e2bf22f77eb3f1efcedb5727e2cc2f2
Burla4enko <45729176+Burla4enko@users.noreply.github.com>
2023-01-22 23:22:59 +0000

    Deploying to gh-pages from @ Burla4enko/goit-react-pr-05-product@e544d398806b13e1fac66ce410252488ff6b2220 🚀

---
+++ static/js/664.30d56c49.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[664],{664:function(e,n,t){t.r(n),t.d(n,{default:function(){return h}});var r,c=t(168),a=t(444).ZP.main(r||(r=(0,c.Z)(["\n  text-align: center;\n"]))),u=t(184);function h(){return(0,u.jsxs)(a,{children:[(0,u.jsx)("h1",{children:"404"}),(0,u.jsx)("p",{children:"Sorry, we couldn't find that page :("})]})}}}]);
+//# sourceMappingURL=664.30d56c49.chunk.js.map
